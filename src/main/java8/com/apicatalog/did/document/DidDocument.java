16e4834f5f946e57896c5795c839791b98a84178
Filip <filip26@gmail.com>
2022-08-07 17:30:25 +0200

    Fix javadoc

--- src/main/java8/com/apicatalog/did/document/DidDocument.java
+++ src/main/java8/com/apicatalog/did/document/DidDocument.java
@@ -11,10 +11,6 @@ import com.apicatalog.did.DidUrl;
 /**
  * DID Document
  * 
- * @param id 
- * @param controller 
- * @param verificationMethod 
- *
  * @see <a href="https://www.w3.org/TR/did-core/#did-document-properties">DID document properties</a>
  */
 
