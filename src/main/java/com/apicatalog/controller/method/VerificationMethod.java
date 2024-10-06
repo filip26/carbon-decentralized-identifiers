ab4cb2a0092ef2d96c4ae74df8bc0a42aa944637
Filip <filip26@gmail.com>
2024-10-06 02:34:47 +0200

    Annotate VerificationMethod

--- src/main/java/com/apicatalog/controller/method/VerificationMethod.java
+++ src/main/java/com/apicatalog/controller/method/VerificationMethod.java
@@ -3,11 +3,17 @@ package com.apicatalog.controller.method;
 import java.net.URI;
 import java.time.Instant;
 
+import com.apicatalog.linkedtree.orm.Fragment;
+import com.apicatalog.linkedtree.orm.Term;
+import com.apicatalog.linkedtree.orm.Vocab;
+
 /**
  * Represents a verification method declaration.
  * 
  * https://www.w3.org/TR/controller-document/#verification-methods
  */
+@Fragment
+@Vocab("https://w3id.org/security#")
 public interface VerificationMethod {
 
     URI id();
@@ -17,5 +23,8 @@ public interface VerificationMethod {
     URI controller();
     
     Instant revoked();
+
+    @Term("expiration")
+    Instant expires();
 
 }
