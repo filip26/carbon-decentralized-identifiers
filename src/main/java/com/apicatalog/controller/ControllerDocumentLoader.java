97c47c5b8e5ece3e9ad1a0d933a891ef43f6a784
Filip <filip26@gmail.com>
2024-11-03 02:01:58 +0100

    Add did-v1.jsonld context

--- src/main/java/com/apicatalog/controller/ControllerDocumentLoader.java
+++ src/main/java/com/apicatalog/controller/ControllerDocumentLoader.java
@@ -16,7 +16,7 @@ import com.apicatalog.jsonld.loader.DocumentLoaderOptions;
 public class ControllerDocumentLoader implements DocumentLoader {
 
     protected static final ControllerDocumentLoader INSTANCE = new ControllerDocumentLoader(null);
-    
+
     protected static final Map<String, Document> staticCache = defaultValues();
 
     protected final DocumentLoader defaultLoader;
@@ -36,7 +36,7 @@ public class ControllerDocumentLoader implements DocumentLoader {
 
         if (defaultLoader != null) {
             return defaultLoader.loadDocument(url, options);
-        } 
+        }
         return null;
     }
 
@@ -47,6 +47,7 @@ public class ControllerDocumentLoader implements DocumentLoader {
         staticCache.put("https://www.w3.org/ns/controller/v1", get("context-v1.jsonld"));
         staticCache.put("https://w3id.org/security/multikey/v1", get("multikey-v1.jsonld"));
         staticCache.put("https://w3id.org/security/jwk/v1", get("jwk-v1.jsonld"));
+        staticCache.put("https://www.w3.org/ns/did/v1", get("did-v1.jsonld"));
 
         return Collections.unmodifiableMap(staticCache);
     }
